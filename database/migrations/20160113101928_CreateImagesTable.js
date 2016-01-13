ba39c7b72ca6900feabb9479eabd3f1913784461
Christian Brel <ch.brel@gmail.com>
2016-01-13 10:45:20 +0100

    refs CMS-4 : Added database migrations to manage Images.

---
+++ database/migrations/20160113101928_CreateImagesTable.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: function(migration, DataTypes) {
+    migration.createTable('Images', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        hashid: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: true
+        },
+        description: {
+          type: DataTypes.STRING,
+          allowNull: true
+        },
+        relDirPath: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.fn('NOW'),
+          allowNull: false
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.fn('NOW'),
+          allowNull: false
+        },
+        ImagesCollectionId : {
+          type: DataTypes.INTEGER,
+          allowNull: true
+        }
+      }
+    );
+  },
+
+  down: function(migration, DataTypes) {
+    migration.dropTable('Images');
+  }
+}
---
+++ database/models/Images.js
@@ -0,0 +1,46 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Images', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    hashid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    relDirPath: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: 'now()'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: 'now()'
+    },
+    ImagesCollectionId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    tableName: 'Images',
+    freezeTableName: true
+  });
+};
