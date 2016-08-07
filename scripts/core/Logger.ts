36566120636de9fc9ba7fa69ae3163203ab95e0f
Christian Brel <ch.brel@gmail.com>
2016-08-07 15:39:59 +0200

    refs CMS-41 : Use winston as Logger.

--- scripts/core/Logger.ts
+++ scripts/core/Logger.ts
@@ -4,12 +4,38 @@
 
 /// <reference path="./LoggerLevel.ts" />
 
+var fs : any = require('fs');
+var winston : any = require('winston');
+var winston_amqp : any = require('winston-amqp');
+var path : any = require('path');
+var amqp : any = require('amqp');
+
 /**
  * Represents a logger with a coloration option.
  *
  * @class Logger
  */
 class Logger {
+	/**
+	 * From.
+	 *
+	 * @property from
+	 * @type string
+	 * @static
+	 * @default true
+	 */
+	static from : string = null;
+
+	/**
+	 * Version.
+	 *
+	 * @property version
+	 * @type string
+	 * @static
+	 * @default true
+	 */
+	static version : string = null;
+
 	/**
 	 * Status of color mode.
 	 *
@@ -31,6 +57,134 @@ class Logger {
 	static level : LoggerLevel = LoggerLevel.Error;
 
 	/**
+	 * Winston Logger.
+	 *
+	 * @property logger
+	 * @type any (Winston Logger)
+	 * @static
+	 * @default null
+	 */
+	static logger : any = null;
+
+	/**
+	 * Return an instance of Winston Logger.
+	 *
+	 * @method getLogger
+	 * @static
+	 */
+	static getLogger() {
+		if(Logger.logger == null) {
+			Logger.buildLogger();
+		}
+
+		return Logger.logger;
+	}
+
+	/**
+	 * Build Winston Logger instance.
+	 *
+	 * @method buildLogger
+	 * @static
+	 */
+	static buildLogger() {
+		Logger.logger = new winston.Logger({
+			exitOnError: false
+		});
+
+		Logger.manageConsoleTransport();
+		Logger.manageAMQPTransport();
+	}
+
+	/**
+	 * Manage Console transport for Winston Logger instance.
+	 *
+	 * @method manageConsoleTransport
+	 * @static
+	 */
+	static manageConsoleTransport() {
+		var options : any = {
+			handleExceptions: true,
+			prettyPrint: true,
+			timestamp : true
+		};
+
+		options["colorize"] = Logger.color;
+
+		switch(Logger.level) {
+			case LoggerLevel.Error :
+				options["level"] = 'error';
+				break;
+			case LoggerLevel.Warning :
+				options["level"] = 'warn';
+				break;
+			case LoggerLevel.Info :
+				options["level"] = 'info';
+				break;
+			case LoggerLevel.Verbose :
+				options["level"] = 'verbose';
+				break;
+			case LoggerLevel.Debug :
+				options["level"] = 'debug';
+				break;
+			default :
+				options["level"] = 'info';
+		}
+
+		if(Logger.logger.transports.length > 0) {
+			if(typeof(Logger.logger.transports["console"]) != "undefined") {
+				Logger.logger.remove(winston.transports.Console);
+			}
+		}
+
+		Logger.logger.add(winston.transports.Console, options);
+	}
+
+	/**
+	 * Manage AMQP transport for Winston Logger instance.
+	 *
+	 * @method manageAMQPTransport
+	 * @static
+	 */
+	static manageAMQPTransport() {
+		if(Logger.logger.transports.length > 0) {
+			if(typeof(Logger.logger.transports["amqp"]) != "undefined") {
+				Logger.logger.remove('amqp');
+			}
+		}
+
+		var connectionSuccess : boolean = false;
+
+		var connection : any = amqp.createConnection({
+			host: 'localhost',
+			port: 5672,
+			vhost: '/',
+			login: 'guest',
+			password: 'guest'
+		}, {
+			reconnect: false
+		});
+
+		connection.on('error', function(err){
+			connection.end();
+
+			if(!connectionSuccess) {
+				console.log("Logger AMQP Transport can't be added.")
+			}
+		});
+
+		connection.on('ready', function () {
+			connectionSuccess = true;
+			connection.end();
+
+			var options : any = {
+				level: 'debug'
+			};
+
+			Logger.logger.add(winston_amqp.AMQP, options);
+		});
+	}
+
+	/**
 	 * Change the color status.
 	 *
 	 * @method useColor
@@ -39,6 +193,7 @@ class Logger {
 	 */
 	static useColor(status : boolean) {
 		Logger.color = status;
+		Logger.buildLogger();
 	}
 
 	/**
@@ -50,6 +205,73 @@ class Logger {
 	 */
 	static setLevel(level : LoggerLevel) {
 		Logger.level = level;
+		Logger.buildLogger();
+	}
+
+	/**
+	 * Retrieve information about running service.
+	 *
+	 * @method retrieveFromVersionInformation
+	 * @static
+	 */
+	static retrieveFromVersionInformation() {
+		try {
+			var stats = fs.statSync(__dirname + '/package.json');
+			var packageJson : any = require(__dirname + '/package.json');
+			Logger.from = packageJson.name;
+			Logger.version = packageJson.version;
+		}
+		catch(err) {
+			Logger.from = "Unknown service";
+			Logger.version = "Unknown version";
+		}
+	}
+
+	/**
+	 * Complete metadata in params with some info like method, line etc... where log was done.
+	 *
+	 * @method completeMetadata
+	 * @static
+	 * @param {any} metadata - Metadata to complete
+	 * @param {boolean} withStack - Boolean to choose to add stack or not.
+	 */
+	static completeMetadata(metadata : any = {}, withStack : boolean = false) {
+		if(Logger.from == null || Logger.version == null) {
+			Logger.retrieveFromVersionInformation();
+		}
+
+		// https://github.com/baryon/tracer/blob/master/lib/console.js
+		var stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/i;
+		var stackReg2 = /at\s+()(.*):(\d*):(\d*)/i;
+
+		var err : any = new Error();
+		var stack = err.stack;
+		var stacksplit = stack.split('\n');
+		var stacklist = stacksplit.slice(3);
+
+		var data : any = {
+			"from" : Logger.from,
+			"version" : Logger.version
+		};
+
+		var s = stacklist[0];
+
+		var sp = stackReg.exec(s) || stackReg2.exec(s);
+
+		if (sp && sp.length === 5) {
+			data.method = sp[1];
+			data.path = sp[2];
+			data.line = sp[3];
+			data.pos = sp[4];
+
+			if(withStack) {
+				data.stack = stacklist.join('\n');
+			}
+		}
+
+		metadata.logDetails = data;
+
+		return metadata;
 	}
 
 	/**
@@ -58,15 +280,22 @@ class Logger {
 	 * @method debug
 	 * @static
 	 * @param {string} msg - The message to log.
+	 * @param {any} metadata - Metadata added to message
 	 */
-	static debug(msg : any) {
-		if (Logger.level === LoggerLevel.Debug) {
-			if (Logger.color && msg != null && msg != undefined && (typeof(msg) == "string" || msg instanceof String)) {
-				console.log(msg.green);
-			} else {
-				console.log(msg);
-			}
-		}
+	static debug(msg : any, metadata : any = {}) {
+		Logger.getLogger().debug(msg, Logger.completeMetadata(metadata));
+	}
+
+	/**
+	 * Log message as Verbose Level.
+	 *
+	 * @method verbose
+	 * @static
+	 * @param {string} msg - The message to log.
+	 * @param {any} metadata - Metadata added to message
+	 */
+	static verbose(msg : any, metadata : any = {}) {
+		Logger.getLogger().verbose(msg, Logger.completeMetadata(metadata));
 	}
 
 	/**
@@ -75,15 +304,10 @@ class Logger {
 	 * @method info
 	 * @static
 	 * @param {string} msg - The message to log.
+	 * @param {any} metadata - Metadata added to message
 	 */
-	static info(msg : any) {
-		if (Logger.level === LoggerLevel.Debug || Logger.level === LoggerLevel.Info) {
-			if (Logger.color && msg != null && msg != undefined && (typeof(msg) == "string" || msg instanceof String)) {
-				console.log(msg.blue);
-			} else {
-				console.log(msg);
-			}
-		}
+	static info(msg : any, metadata : any = {}) {
+		Logger.getLogger().info(msg, Logger.completeMetadata(metadata));
 	}
 
 	/**
@@ -92,15 +316,10 @@ class Logger {
 	 * @method warn
 	 * @static
 	 * @param {string} msg - The message to log.
+	 * @param {any} metadata - Metadata added to message
 	 */
-	static warn(msg : any) {
-		if (Logger.level === LoggerLevel.Debug || Logger.level === LoggerLevel.Info || Logger.level === LoggerLevel.Warning) {
-			if (Logger.color && msg != null && msg != undefined && (typeof(msg) == "string" || msg instanceof String)) {
-				console.log(msg.orange);
-			} else {
-				console.log(msg);
-			}
-		}
+	static warn(msg : any, metadata : any = {}) {
+		Logger.getLogger().warn(msg, Logger.completeMetadata(metadata));
 	}
 
 	/**
@@ -109,13 +328,10 @@ class Logger {
 	 * @method error
 	 * @static
 	 * @param {string} msg - The message to log.
+	 * @param {any} metadata - Metadata added to message
 	 */
-	static error(msg : any) {
-		if(Logger.color && msg != null && msg != undefined && (typeof(msg) == "string" || msg instanceof String)) {
-			console.error(msg.red);
-		} else {
-			console.error(msg);
-		}
+	static error(msg : any, metadata : any = {}) {
+		Logger.getLogger().error(msg, Logger.completeMetadata(metadata, true));
 	}
 
 }
